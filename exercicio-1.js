e34f376c87cfcb5db973995cc5d2e7433336bfb6
Rodrigo Perez <101291823+roduperez@users.noreply.github.com>
2022-09-02 19:57:00 -0300

    exercicio-1

---
+++ exercicio-1.js
@@ -0,0 +1,11 @@
+const name = 'Rodrigo Perez'
+const idade = 46
+const profissao = 'Técnico em Telecomunicações'
+const hobbit = 'ver filmes, brincar com minhas filhas'
+const esporte = 'Judô'
+const time = 'SPFC'
+
+console.log('Meu nome é ' + name + ' e tenho ' + idade + ' anos.')
+console.log('Minha profissão é ' + profissao +'.')
+console.log('No meu tempo livre gosto de ' + hobbit + '.')
+console.log('Já pratiquei ' + esporte + ' e meu time de futebol é o ' + time + '.')
